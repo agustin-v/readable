5f32d37dc608752c8aa50e99af9663966beb63c4
Agustin Villalobos <guxtiv@gmail.com>
2017-12-29 09:24:14 -0600

    Created Comment component

---
+++ src/components/Comment/Comment.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CSSModules from 'react-css-modules'
+import styles from './comment.css'
+
+const Comment = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default CSSModules(Comment, styles)
