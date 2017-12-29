f128bbe6fb90f39889b03e5972e169df018f2801
Agustin Villalobos <guxtiv@gmail.com>
2017-12-28 21:06:53 -0600

    Added ActionBar to HomeContainer

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 
 import Navbar from '../components/Navbar/Navbar'
+import ActionBar from '../components/ActionBar/ActionBar'
 
 class HomeContainer extends Component {
     render() {
         return(
             <div>
                 {Navbar()}
+                {ActionBar({label: 'home', action:'sort'})}
             </div>
         )
     }
