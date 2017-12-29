a834710098080c416916aaa87a0cacebf0ade6d1
Agustin Villalobos <guxtiv@gmail.com>
2017-12-29 09:24:38 -0600

    NewComment created

---
+++ src/components/NewComment/NewComment.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CSSModules from 'react-css-modules'
+import styles from './newComment.css'
+
+const NewComment = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default CSSModules(NewComment, styles)
