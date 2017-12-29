ae6988fcdfbb5d5464e04f52116ea088195aab02
Agustin Villalobos <guxtiv@gmail.com>
2017-12-29 16:38:44 -0600

    Comment component

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -4,8 +4,27 @@ import styles from './comment.css'
 
 const Comment = () => {
     return(
-        <div></div>
+        <div styleName='wrapper'>
+            <div styleName='header'>
+                <div styleName='info'>
+                    <div styleName='author'>
+                        Jane Doe
+                    </div>
+                    <div>
+                        <span className="lnr lnr-calendar-full"></span>
+                        <span styleName='date'>20/07/18</span>
+                    </div>
+                </div>
+                <div styleName='actions'>
+                    <span styleName='actions-space actionable' className="lnr lnr-pencil"></span>
+                    <span styleName='actionable' className="lnr lnr-cross"></span>
+                </div>
+            </div>
+            <div styleName='comment'>
+                
+            </div>
+        </div>
     )
 }
 
-export default CSSModules(Comment, styles)
+export default CSSModules(Comment, styles, {allowMultiple: true})
