c9d1f9c521bef49a0aca0a80bbd48f061e679fd3
Agustin Villalobos <guxtiv@gmail.com>
2017-12-29 12:40:29 -0600

    Post preview component created

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -4,7 +4,30 @@ import styles from './preview.css'
 
 const Preview = () => {
     return(
-        <div></div>
+        <div styleName='wrapper'>
+            <div styleName='title'>
+                Just kidding. It takes more than 10 minutes to learn technology.
+            </div>
+            <div styleName='author'>
+                jane doe
+            </div>
+            <div styleName='info-wrapper'>
+                <div styleName='info-left'>
+                    <div styleName='info-first'>
+                        <span styleName='info-icon' className="lnr lnr-thumbs-up"></span>
+                        <span styleName='info-text'>20</span>
+                    </div>
+                    <div>
+                        <span styleName='info-icon' className="lnr lnr-bubble"></span>
+                        <span styleName='info-text'>9</span>
+                    </div>
+                </div>
+                <div styleName='info-right'>
+                    <span styleName='info-icon' className="lnr lnr-calendar-full"></span>
+                    <span styleName='info-text'>20/07/18</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
