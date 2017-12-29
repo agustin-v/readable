d46e47af928fa2385dd722a72343e5e58aec3ba9
Agustin Villalobos <guxtiv@gmail.com>
2017-12-29 09:25:33 -0600

    Preview component created

---
+++ src/components/Preview/Preview.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CSSModules from 'react-css-modules'
+import styles from './preview.css'
+
+const Preview = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default CSSModules(Preview, styles)
