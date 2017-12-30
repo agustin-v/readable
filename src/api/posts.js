542733b5ce807c229ad2c75120919719d70aa217
Agustin Villalobos <guxtiv@gmail.com>
2017-12-30 16:49:20 -0600

    implemented axios to call the api

--- src/api/posts.js
+++ src/api/posts.js
@@ -3,11 +3,57 @@ import utils from '../utils/api'
 
 class PostsApi {
     static authorizationHeaders(){
-        return {}
+        return {Authorization: 1234565432}
     }
 
     static getAll(){
-        
+        const url = `${process.env.REACT_APP_BASE_URL}/posts`
+        return axios(
+            {
+                headers: this.authorizationHeaders(),
+                method: 'get',
+                url:url
+            }
+        )
+        .then((response) => utils.checkStatus(response))
+        .then((response) => utils.parseJSON(response))
+        .catch((error) => {
+            throw error
+        })
+    }
+
+    static getCategory(category){
+        const url = `${process.env.REACT_APP_BASE_URL}/${category}/posts`
+
+        return axios(
+            {
+                headers: this.authorizationHeaders(),
+                method: 'get',
+                url:url
+            }
+        )
+        .then((response) => utils.checkStatus(response))
+        .then((response) => utils.parseJSON(response))
+        .catch((error) => {
+            throw error
+        })
+    }
+
+    static getOne(id){
+        const url = `${process.env.REACT_APP_BASE_URL}/posts/${id}`
+
+        return axios(
+            {
+                headers: this.authorizationHeaders(),
+                method: 'get',
+                url:url
+            }
+        )
+        .then((response) => utils.checkStatus(response))
+        .then((response) => utils.parseJSON(response))
+        .catch((error) => {
+            throw error
+        })
     }
 }
 
