ad5224ee6e48c9bd8db4391df09e7b73c4b7b278
Agustin Villalobos <guxtiv@gmail.com>
2017-12-30 16:50:07 -0600

    implemented the variables from the state

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -1,30 +1,31 @@
 import React from 'react'
 import CSSModules from 'react-css-modules'
 import styles from './preview.css'
+import moment from 'moment'
 
-const Preview = () => {
+const Preview = ({title, author, id, commentCount, voteScore, timestamp }) => {
     return(
         <div styleName='wrapper'>
             <div styleName='title'>
-                Just kidding. It takes more than 10 minutes to learn technology.
+                {title}
             </div>
             <div styleName='author'>
-                jane doe
+                {author}
             </div>
             <div styleName='info-wrapper'>
                 <div styleName='info-left'>
                     <div styleName='info-first'>
                         <span styleName='info-icon' className="lnr lnr-thumbs-up"></span>
-                        <span styleName='info-text'>20</span>
+                        <span styleName='info-text'>{voteScore}</span>
                     </div>
                     <div>
                         <span styleName='info-icon' className="lnr lnr-bubble"></span>
-                        <span styleName='info-text'>9</span>
+                        <span styleName='info-text'>{commentCount}</span>
                     </div>
                 </div>
                 <div styleName='info-right'>
                     <span styleName='info-icon' className="lnr lnr-calendar-full"></span>
-                    <span styleName='info-text'>20/07/18</span>
+                    <span styleName='info-text'>{moment(timestamp).format('DD/MM/YY')}</span>
                 </div>
             </div>
         </div>
