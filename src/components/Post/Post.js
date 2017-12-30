30fe6bdfdbdc74a1e32d4f7574496380215fa10b
Agustin Villalobos <guxtiv@gmail.com>
2017-12-30 15:07:58 -0600

    styles for components

---
+++ src/components/Post/Post.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import CSSModules from 'react-css-modules'
+import styles from './post.css'
+
+const Post = () => {
+    return(
+        <div styleName='wrapper'>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default CSSModules(Post, styles)
