5d354b8a345d917cd94b937d70855fea3e35b254
Agustin Villalobos <guxtiv@gmail.com>
2018-01-08 21:18:29 -0600

    added an action to call the current post based on the id in the URL

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -11,6 +11,20 @@ export function getAllPosts() {
     }
 }
 
+export function getCurrentPost(id) {
+    return (dispatch) => {
+        return API.getOne(id).then( post => {
+            dispatch(setCurrentPost(post))
+        }).catch( err => {
+            throw err;
+        })
+    }
+}
+
 export function setPosts(posts) {
     return {type: types.SET_POSTS, posts}
+}
+
+export function setCurrentPost(currentPost) {
+    return{type: types.SET_CURRENT_POST, currentPost}
 }
