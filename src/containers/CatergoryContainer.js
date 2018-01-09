c4d2ce8626e8dd31e8ca1cfd4b6911a80d8b10b4
Agustin Villalobos <guxtiv@gmail.com>
2018-01-08 21:16:28 -0600

    Used URL param to filter the posts based on its category

--- src/containers/CatergoryContainer.js
+++ src/containers/CatergoryContainer.js
@@ -1,19 +1,47 @@
 import React, {Component} from 'react'
-
+import CSSModules from 'react-css-modules'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import styles from '../styles/modules/homeContainer.css'
+import * as SortingActions from '../actions/sortingActions'
+import * as PostsActions from '../actions/postsActions'
 import Navbar from '../components/Navbar/Navbar'
 import ActionBar from '../components/ActionBar/ActionBar'
 import Categorybar from '../components/CategoryBar/CategoryBar'
+import PostsContainer from './PostsContainer'
 
 class CategoryContainer extends Component {
+
     render(){
+        const { posts, votes, time } = this.props;
+        let filterPosts = posts.filter(post => post.category === this.props.match.params.category)
         return(
             <div>
-                {Navbar()}
-                {ActionBar({ label: 'activeCategory', action:'sort', back: true })}
-                {Categorybar()}
+                <div>
+                    {Navbar()}
+                    {ActionBar({ label: this.props.match.params.category, action:'sort', back: true, votes, time })}
+                </div>
+                <div styleName='main'>
+                    {Categorybar()}
+                    <PostsContainer posts={filterPosts}/>
+                </div>
             </div>
         )
     }
 }
 
-export default CategoryContainer
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts,
+        votes: state.sorting.votes,
+        time: state.sorting.time,
+    }
+}
+
+function  mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({ ...PostsActions, ...SortingActions }, dispatch)
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CSSModules(CategoryContainer, styles))
