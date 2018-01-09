e0a1460db1344ed3fa3f27584a2f6fe7beaf1452
Agustin Villalobos <guxtiv@gmail.com>
2018-01-08 21:25:26 -0600

    Added variables author, title and body. *Pending:* styles

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,20 +2,23 @@ import React from 'react'
 import CSSModules from 'react-css-modules'
 import styles from './post.css'
 
-const Post = () => {
-    return(
-        <div styleName='wrapper'>
-            <div>
-
+const Post = ({currentPost}) => {
+    if(currentPost !== undefined){
+        return(
+            <div styleName='wrapper'>
+                <div>
+                    {currentPost.title}
+                </div>
+                <div>
+                    {currentPost.author}
+                </div>
+                <div>
+                    {currentPost.body}
+                </div>
             </div>
-            <div>
-
-            </div>
-            <div>
-
-            </div>
-        </div>
-    )
+        )
+    }
+    
 }
 
 export default CSSModules(Post, styles)
